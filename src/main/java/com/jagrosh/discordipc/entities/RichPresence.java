9f29a6c353349d2cebb482cfef10d67a6fa03613
hapily04 <ryanbauwens1@gmail.com>
2022-06-20 01:35:19 -0500

    Fixed empty buttons array being passed in

--- src/main/java/com/jagrosh/discordipc/entities/RichPresence.java
+++ src/main/java/com/jagrosh/discordipc/entities/RichPresence.java
@@ -108,6 +108,7 @@ public class RichPresence
                     .put("url", e.getValue())
             );
         }
+        System.out.println(a.length());
         if(a.length() == 0) {
             return null;
         }
